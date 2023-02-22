5c2664ea345cb3faacc1c6b0471708c596416263
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-02-22 15:01:32 +0100

    FEAT+STYLE content and cicle v-for added

--- js/app.js
+++ js/app.js
@@ -3,7 +3,140 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        contacts: [
+            {
+                name: 'Pippo',
+                avatar: 'https://upload.wikimedia.org/wikipedia/it/f/fc/Pippo_Disney.png',
+                visible: true,
+                messages: [
+                    {
+                        date: '22/02/2023 14:40:02',
+                        message: 'Ciao Pippo',
+                        status: 'sent'
+                    },
+                    {
+                        date: '22/02/2023 14:40:12',
+                        message: 'Come va?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '22/02/2023 14:48:54',
+                        message: 'Male, sto morendo',
+                        status: 'received'
+                    },
+                ]
+            },
+            {
+                name: 'Pippo',
+                avatar: 'https://upload.wikimedia.org/wikipedia/it/f/fc/Pippo_Disney.png',
+                visible: true,
+                messages: [
+                    {
+                        date: '22/02/2023 14:40:02',
+                        message: 'Ciao Pippo',
+                        status: 'sent'
+                    },
+                    {
+                        date: '22/02/2023 14:40:12',
+                        message: 'Come va?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '22/02/2023 14:48:54',
+                        message: 'Male, sto morendo',
+                        status: 'received'
+                    },
+                ]
+            },
+            {
+                name: 'Pippo',
+                avatar: 'https://upload.wikimedia.org/wikipedia/it/f/fc/Pippo_Disney.png',
+                visible: true,
+                messages: [
+                    {
+                        date: '22/02/2023 14:40:02',
+                        message: 'Ciao Pippo',
+                        status: 'sent'
+                    },
+                    {
+                        date: '22/02/2023 14:40:12',
+                        message: 'Come va?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '22/02/2023 14:48:54',
+                        message: 'Male, sto morendo',
+                        status: 'received'
+                    },
+                ]
+            },
+            {
+                name: 'Pippo',
+                avatar: 'https://upload.wikimedia.org/wikipedia/it/f/fc/Pippo_Disney.png',
+                visible: true,
+                messages: [
+                    {
+                        date: '22/02/2023 14:40:02',
+                        message: 'Ciao Pippo',
+                        status: 'sent'
+                    },
+                    {
+                        date: '22/02/2023 14:40:12',
+                        message: 'Come va?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '22/02/2023 14:48:54',
+                        message: 'Male, sto morendo',
+                        status: 'received'
+                    },
+                ]
+            },
+            {
+                name: 'Pippo',
+                avatar: 'https://upload.wikimedia.org/wikipedia/it/f/fc/Pippo_Disney.png',
+                visible: true,
+                messages: [
+                    {
+                        date: '22/02/2023 14:40:02',
+                        message: 'Ciao Pippo',
+                        status: 'sent'
+                    },
+                    {
+                        date: '22/02/2023 14:40:12',
+                        message: 'Come va?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '22/02/2023 14:48:54',
+                        message: 'Male, sto morendo',
+                        status: 'received'
+                    },
+                ]
+            },
+            {
+                name: 'Pippo',
+                avatar: 'https://upload.wikimedia.org/wikipedia/it/f/fc/Pippo_Disney.png',
+                visible: true,
+                messages: [
+                    {
+                        date: '22/02/2023 14:40:02',
+                        message: 'Ciao Pippo',
+                        status: 'sent'
+                    },
+                    {
+                        date: '22/02/2023 14:40:12',
+                        message: 'Come va?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '22/02/2023 14:48:54',
+                        message: 'Male, sto morendo',
+                        status: 'received'
+                    },
+                ]
+            },
+        ]
       }
     }
   }).mount('#app')
