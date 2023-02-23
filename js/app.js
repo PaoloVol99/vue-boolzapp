6aa7d3332c0c37beb0730c5878f43a143628707a
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-02-23 16:07:58 +0100

    FEAT Milestone 2 and 3.1 completed, added Luxon

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+const DateTime = luxon.DateTime;
+const now = DateTime.now();
+
 const { createApp } = Vue
 
   createApp({
@@ -5,8 +8,8 @@ const { createApp } = Vue
       return {
         contacts: [
             {
-                name: 'Boolippo',
-                avatar: 'https://upload.wikimedia.org/wikipedia/it/f/fc/Pippo_Disney.png',
+                name: 'Pippo',
+                avatar: './img/pippo.png',
                 visible: true,
                 messages: [
                     {
@@ -27,116 +30,144 @@ const { createApp } = Vue
                 ]
             },
             {
-                name: 'Boolippo',
-                avatar: 'https://upload.wikimedia.org/wikipedia/it/f/fc/Pippo_Disney.png',
+                name: 'Batman',
+                avatar: './img/batman.jpg',
                 visible: true,
                 messages: [
                     {
                         date: '22/02/2023 14:40:02',
-                        message: 'Ciao Pippo',
+                        message: 'Hey Batman',
                         status: 'sent'
                     },
                     {
                         date: '22/02/2023 14:40:12',
-                        message: 'Come va?',
-                        status: 'sent'
+                        message: 'Dimmi',
+                        status: 'received'
                     },
                     {
                         date: '22/02/2023 14:48:54',
-                        message: 'Male, la mia ora è giunta',
+                        message: 'Ci sei per una partita di calcetto? Ci manca il decimo',
+                        status: 'sent'
+                    },
+                    {
+                        date: '22/02/2023 14:49:24',
+                        message: 'Credo che tu abbia sbagliato numero',
                         status: 'received'
                     },
                 ]
             },
             {
-                name: 'Boolippo',
-                avatar: 'https://upload.wikimedia.org/wikipedia/it/f/fc/Pippo_Disney.png',
+                name: 'Joel Miller',
+                avatar: './img/joel.jpeg',
                 visible: true,
                 messages: [
                     {
                         date: '22/02/2023 14:40:02',
-                        message: 'Ciao Pippo',
+                        message: 'Joel, mi ha scritto uno zombie, ha detto "Graeajdhowdiwaoidhuwadaaaah", ne sai qualcosa?',
                         status: 'sent'
                     },
                     {
                         date: '22/02/2023 14:40:12',
-                        message: 'Come va?',
+                        message: 'Non mi far perdere tempo, solo uno zombie di the Walking Dead direbbe una cosa del genere. Niente a che fare con me',
+                        status: 'received'
+                    },
+                    {
+                        date: '22/02/2023 14:48:54',
+                        message: 'Ah ok, scusa per il disturbo',
+                        status: 'sent'
+                    },
+                ]
+            },
+            {
+                name: 'Zombie di The Walking Dead',
+                avatar: './img/zombie.jpeg',
+                visible: true,
+                messages: [
+                    {
+                        date: '22/02/2023 14:40:02',
+                        message: 'Graeajdhowdiwaoidhuwadaaaah',
+                        status: 'received'
+                    },
+                    {
+                        date: '22/02/2023 14:40:12',
+                        message: 'Come scusa? Non ho ben capito',
                         status: 'sent'
                     },
                     {
                         date: '22/02/2023 14:48:54',
-                        message: 'Male, la mia ora è giunta',
+                        message: 'Graeajdhowdiwaoidhuwadaaaah',
                         status: 'received'
                     },
                 ]
             },
             {
-                name: 'Boolippo',
-                avatar: 'https://upload.wikimedia.org/wikipedia/it/f/fc/Pippo_Disney.png',
+                name: 'Michael Scott',
+                avatar: './img/michael.webp',
                 visible: true,
                 messages: [
                     {
                         date: '22/02/2023 14:40:02',
-                        message: 'Ciao Pippo',
+                        message: 'Hey Michael, what\'s your secret?',
                         status: 'sent'
                     },
                     {
                         date: '22/02/2023 14:40:12',
-                        message: 'Come va?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '22/02/2023 14:48:54',
-                        message: 'Male, la mia ora è giunta',
+                        message: 'My philosophy is, basically this. And this is something that I live by. And I always have. And I always will. Don’t, ever, for any reason, do anything, to anyone, for any reason, ever, no matter what, no matter where, or who you are with, or, or where you are going, or, or where you’ve been. Ever. For any reason. Whatsoever.”',
                         status: 'received'
                     },
                 ]
             },
             {
-                name: 'Boolippo',
-                avatar: 'https://upload.wikimedia.org/wikipedia/it/f/fc/Pippo_Disney.png',
+                name: 'Dwight Schrute',
+                avatar: './img/dwight.jpeg',
                 visible: true,
                 messages: [
                     {
-                        date: '22/02/2023 14:40:02',
-                        message: 'Ciao Pippo',
-                        status: 'sent'
-                    },
-                    {
-                        date: '22/02/2023 14:40:12',
-                        message: 'Come va?',
-                        status: 'sent'
-                    },
-                    {
                         date: '22/02/2023 14:48:54',
-                        message: 'Male, la mia ora è giunta',
+                        message: 'Whenever I\'m about to do something, I think, "Would an idiot do that?" And if they would, I do not do that thing.',
                         status: 'received'
                     },
                 ]
             },
-            {
-                name: 'Boolippo',
-                avatar: 'https://upload.wikimedia.org/wikipedia/it/f/fc/Pippo_Disney.png',
-                visible: true,
-                messages: [
-                    {
-                        date: '22/02/2023 14:40:02',
-                        message: 'Ciao Pippo',
-                        status: 'sent'
-                    },
-                    {
-                        date: '22/02/2023 14:40:12',
-                        message: 'Come va?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '22/02/2023 14:48:54',
-                        message: 'Male, la mia ora è giunta',
-                        status: 'received'
-                    },
-                ]
-            },
-        ]
+        ],
+        activeContactIndex: 0,
+        previewLength: 55,
+        lastIndex: null,
+        newMessage: '',
+        DateTime: DateTime,
+        now: now
       }
+    },
+    methods: {
+        activeContact(index) {
+            this.activeContactIndex = index
+        },
+        lastMessage(index) {
+            this.lastIndex = this.contacts[index].messages.length - 1
+            return this.lastIndex
+        },
+        cutMessage(string) {
+            if (string.length > this.previewLength) {
+                return string.substring(0, this.previewLength) + '...'
+            } else {
+                return string
+            }
+        },
+        sendMessage() {
+            console.log('enter')
+            let newDate = this.now.toFormat('dd/MM/yyyy HH:mm:ss')
+            this.contacts[this.activeContactIndex].messages.push(
+                {
+                    date: newDate,
+                    message: this.newMessage,
+                    status: 'sent'
+                }
+            )
+            this.newMessage = '' 
+        },
+        dateToHHmm(string) {
+            const dT = this.DateTime.fromFormat(string, 'dd/MM/yyyy HH:mm:ss')
+            return dT.toFormat('HH:mm')
+        }
     }
   }).mount('#app')
